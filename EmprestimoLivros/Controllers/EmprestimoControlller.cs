868520cd55ca851f6083390ec96930f028e97551
triz0 <triz0@NITROV15_DA_BIA>
2026-09-08 13:14:30 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Controllers/EmprestimoControlller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoControlller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
